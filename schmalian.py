8fb5d3292544cb772fdac0cf2071947909179c7e
Gabriel <gabrielrr96@gmail.com>
2022-02-08 12:55:55 -0300

    First commit in ICAS.

--- schmalian.py
+++ schmalian.py
@@ -180,7 +180,81 @@ def plot_energy_bands():
     ax.legend()
     fig.savefig("C:\\Users\\gabri\\OneDrive\\Doctorado\\Python\\Tritops\\Images\\Energy_bands.png")
     
-plot_energy_bands()
+#plot_energy_bands()
+
+def hopping_Josephson_pm(site1, site2, t, Delta, phi):
+    if (site1.pos == [0.0] and site2.pos == [-1.0]) or (site1.pos == [-1.0] and site2.pos == [0.0]):
+        return 2*t * (np.kron((np.eye(2)+tau_z)/2, np.eye(2))*np.exp(1j*phi/2)
+                    + np.kron((np.eye(2)-tau_z)/2, np.eye(2))*np.exp(-1j*phi/2))
+    else:
+        return ( -t * np.kron(tau_z, np.eye(2)) +
+                1j * Delta * np.kron(tau_x, sigma_y) )
+        
+
+def make_Josephson_junction_pm(t=1, mu=0, Delta=1, L=25, phi=0):
+    """
+    Create a 1D tight-binding model for
+    Josephson's junction with magnetic flux.
+    
+    Parameters
+    ----------
+    t : float, optional
+        Hopping parameter. The default is 1.
+    mu : float optional
+        Chemical potential. The default is 1.
+    Delta : float, optional
+        Superconducting gap. The default is 1.
+    L : int, optional
+        Number of sites in the chain. The default is 25.
+    phi : float, optional
+        Magnetic flux in units of quantum flux h/(2e).
+    
+    Returns
+    -------
+    kwant.builder.Builder
+        The representation for the tight-binding model.
+    """
+    Josephson_junction_pm = kwant.Builder()
+    lat = kwant.lattice.chain(norbs=2)  
+    # The superconducting order parameter couples electron and hole orbitals
+    # on each site, and hence enters as an onsite potential
+    # There are L sites in each superconductor
+    Josephson_junction_pm[(lat(x) for x in range(-L, L))] = onsite
+    # Hoppings
+    Josephson_junction_pm[lat.neighbors()] = hopping_Josephson_pm
+    # The hopping in the contact has a phase dependence.
+    #kitaev_chain_finite[lat(-1), lat(0)] = hopping_contact
+    #kitaev_chain_finite[lat(0), lat(-1)] = hopping_contact
+    #kwant.plot(kitaev_chain_finite)
+    return Josephson_junction_pm
+
+def Josephson_current(syst, params):
+    fundamental_energy = []
+    for phi in params["phi"]:
+        params["phi"] = phi
+        H = syst.hamiltonian_submatrix(params=params)
+        eigenvalues, eigenvectors = np.linalg.eig(H)
+        fundamental_energy.append(-np.sum(eigenvalues, where=eigenvalues>0) / 2)
+    current = np.diff(fundamental_energy)
+    return current
+
+def plot_spectrum(syst, phi, params):
+    """
+    Plot the spectrum by changing the parameter 'phi'.
+
+    Parameters
+    ----------
+    syst : kwant.builder.FiniteSystem
+        Finite Kitaev chain.
+    phi : np.array
+        Phase difference between superconductors.
+
+    """
+    fig = kwant.plotter.spectrum(syst, ("phi", phi), params=params)
+    fig.canvas.manager.set_window_title("Bandas de energía de una cadena finita")    
+    plt.title("Bandas")
+    plt.xlabel(r"$\frac{\phi}{\phi_0}$")
+    plt.ylabel(r"Energía")
 
 #%%
 def main():

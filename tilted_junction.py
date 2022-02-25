bc5e214cdcca8cd52d5a6efde098d6573a518096
Gabriel <gabrielrr96@gmail.com>
2022-02-25 17:58:26 -0300

    New plotting functions.

--- tilted_junction.py
+++ tilted_junction.py
@@ -94,6 +94,58 @@ def plot_spectrum(syst, phi, params, ax=None):
     """
     kwant.plotter.spectrum(syst, ("phi", phi), params=params, ax=ax)
 
+def plot_k_resolved_current(t, t_J, mu, Delta_0, Delta_1, lambda_R,
+                            theta, L, k):
+    fig, ax = plt.subplots(dpi=300)
+    ax.set_title(rf"k-resolved Josephson current for H_ZKM, $\theta$={theta:.2f}")
+    ax.set_xlabel(r"$\phi$")
+    ax.set_ylabel(r"$J_k$")
+    ax.set_xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi]),
+    ax.set_xticklabels([r"0", r"$\frac{\pi}{2}$", "$\pi$", r"$\frac{3\pi}{2}$","$2\pi$"])
+    ax.grid()
+    plt.tight_layout()
+    ribbon_ZKM = make_Josephson_junction_ZKM(mu=mu, L=L)
+    ribbon_ZKM = ribbon_ZKM.finalized()
+    phi = np.linspace(0, 2*np.pi, 100)
+    total_current = []
+    for k_value in k:
+        params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1,
+                      lambda_R=lambda_R, L=L, phi=phi, k=k_value,
+                      t_J=t_J, theta=theta)
+        current = Josephson_current(ribbon_ZKM, params)
+        total_current.append(current)
+        if k_value in [-np.pi, -np.pi/2, 0, np.pi/2, np.pi]:
+            ax.plot(phi[:-1], current, label=f"{k_value:.2f}")
+        else:
+            ax.plot(phi[:-1], current, label="_nolegend_")
+    plt.legend(loc="upper right")
+    return fig, ax
+    
+def plot_total_current(t, t_J, mu, Delta_0, Delta_1, lambda_R,
+                            theta, L, k):
+    fig, ax = plt.subplots(dpi=300)
+    ax.set_title(rf"Total Josephson current for H_ZKM, L={L}")
+    ax.set_xlabel(r"$\phi$")
+    ax.set_ylabel(r"$J_k$")
+    ax.set_xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi]),
+    ax.set_xticklabels([r"0", r"$\frac{\pi}{2}$", "$\pi$", r"$\frac{3\pi}{2}$","$2\pi$"])
+    ax.grid()
+    plt.tight_layout()
+    ribbon_ZKM = make_Josephson_junction_ZKM(mu=mu, L=L)
+    ribbon_ZKM = ribbon_ZKM.finalized()
+    phi = np.linspace(0, 2*np.pi, 100)
+    for theta_value in theta:
+        total_current = []
+        for k_value in k:
+            params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1,
+                          lambda_R=lambda_R, L=L, phi=phi, k=k_value,
+                          t_J=t_J, theta=theta_value)
+            current = Josephson_current(ribbon_ZKM, params)
+            total_current.append(current)
+        total_current = np.sum(total_current, axis=0) / len(k)
+        ax.plot(phi[:-1], total_current, label=rf"$\theta={theta_value:.2f}$")
+    plt.legend(loc="upper right")
+    return fig, ax
 
 def main():
     # without crossing 
@@ -112,32 +164,20 @@ def main():
     # Delta_1 = 0.4*t
     # lambda_R = 0.5*t
     
-    theta = np.pi/2
+    theta = 0
     L = 50
-    fig, ax = plt.subplots(dpi=300)
-    ax.set_title(rf"k-resolved Josephson current for H_ZKM, $\theta$={theta:.2f}")
-    ax.set_xlabel(r"$\phi$")
-    ax.set_ylabel(r"$J_k$")
-    ax.set_xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi]),
-    ax.set_xticklabels([r"0", r"$\frac{\pi}{2}$", "$\pi$", r"$\frac{3\pi}{2}$","$2\pi$"])
-    ax.grid()
-    plt.tight_layout()
-    ribbon_ZKM = make_Josephson_junction_ZKM(mu=mu, L=L)
-    ribbon_ZKM = ribbon_ZKM.finalized()
-    phi = np.linspace(0, 2*np.pi, 100)
-    k = np.linspace(-np.pi, np.pi, 10)
-    k = np.append(k, [-np.pi/2, 0, np.pi/2])
-    for k_value in k:
-        params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1,
-                      lambda_R=lambda_R, L=L, phi=phi, k=k_value,
-                      t_J=t_J, theta=theta)
-        current = Josephson_current(ribbon_ZKM, params)
-        if k_value in [-np.pi, -np.pi/2, 0, np.pi/2, np.pi]:
-            ax.plot(phi[:-1], current, label=f"{k_value:.2f}")
-        else:
-            ax.plot(phi[:-1], current, label="_nolegend_")
-    plt.legend(loc="upper right")
-    fig.savefig(os.getcwd()+f"/Images/Josephson_H_ZKM_crossing_L={L}_theta={theta:.2f}.png")
+    k = np.linspace(-np.pi, -np.pi+0.5, 10)
+    params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1,
+                  lambda_R=lambda_R, L=L,
+                  t_J=t_J, theta=theta)
+    
+    #fig, ax = plot_k_resolved_current(k=k, **params)
+    #fig.savefig(os.getcwd()+f"/Images/Tilted/Josephson_H_ZKM_crossing_L={L}_theta={theta:.2f}.png")
+
+    params.pop("theta")
+    theta = [0, np.pi/4, np.pi/2, np.pi, 3*np.pi/2, 7*np.pi/4]
+    fig, ax = plot_total_current(k=k, theta=theta, **params)
+    fig.savefig(os.getcwd()+f"/Images/Tilted/Total_Josephson_H_ZKM_without_crossing_L={L}.png")
 
 if __name__ == '__main__':
     main()
--- tritops_tritops.py
+++ tritops_tritops.py
@@ -123,22 +123,22 @@ def make_Josephson_junction_ZKM(t=1, mu=0, Delta=1, L=25, phi=0, t_J=1):
 
 def main():
     # without crossing 
-    t = 1
-    t_J = t/2
-    mu = 2*t
-    Delta_0 = 0.4*t
-    Delta_1 = 0.2*t
-    lambda_R = 0.5*t
-    
-    # with crossing
     # t = 1
-    # t_J = t
-    # mu = t
+    # t_J = t/2
+    # mu = 2*t
     # Delta_0 = 0.4*t
-    # Delta_1 = 0.4*t
+    # Delta_1 = 0.2*t
     # lambda_R = 0.5*t
     
-    L = 10
+    # with crossing
+    t = 1
+    t_J = t
+    mu = t
+    Delta_0 = 0.4*t
+    Delta_1 = 0.4*t
+    lambda_R = 0.5*t
+    
+    L = 50
     fig, ax = plt.subplots(dpi=300)
     ax.set_title("k-resolved Josephson current for H_ZKM")
     ax.set_xlabel(r"$\phi$")
@@ -151,30 +151,35 @@ def main():
     #kwant.plot(syst, site_color=site_color, hop_color=hop_color)
     ribbon_ZKM = ribbon_ZKM.finalized()
     phi = np.linspace(0, 2*np.pi, 100)
-    for k in np.linspace(-np.pi, np.pi, 10):
-        params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1, lambda_R=lambda_R, L=L, phi=phi, k=k, t_J=t_J)
-        #plot_spectrum(kitaev, mu)
+    k = np.linspace(-np.pi, -np.pi+0.5, 10)
+    for k_value in k:
+        params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1, lambda_R=lambda_R, L=L, phi=phi, k=k_value, t_J=t_J)
         current = Josephson_current(ribbon_ZKM, params)
         ax.plot(phi[:-1], current)
+        if k_value in [-np.pi, -np.pi/2, 0, np.pi/2, np.pi]:
+            ax.plot(phi[:-1], current, label=f"{k_value:.2f}")
+        else:
+            ax.plot(phi[:-1], current, label="_nolegend_")
+    plt.legend(loc="upper right")
         #ax.plot(phi[:-1], current, label=f"{k:.2f}")
     #plt.legend(loc="upper right")
     #fig.savefig(os.getcwd()+f"/Images/Josephson_H_ZKM_crossing_L={L}")
     
-    k = np.pi
-    fig, ax = plt.subplots(dpi=300)
-    ax.set_title(f"Spectrum for H0 and k={k}")
-    ax.set_xlabel(r"$\phi$")
-    ax.set_ylabel(r"$E(\phi)$")
-    ax.set_xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi]),
-    ax.set_xticklabels([r"0", r"$\frac{\pi}{2}$", "$\pi$", r"$\frac{3\pi}{2}$","$2\pi$"])
-    ax.grid()
-    plt.tight_layout()
-    ribbon_ZKM = make_Josephson_junction_ZKM(mu=mu, L=L)
-    ribbon_ZKM = ribbon_ZKM.finalized()
-    params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1, lambda_R=lambda_R, L=L, phi=phi, k=0, t_J=t_J)
-    phi = np.linspace(0, 2*np.pi, 50)
-    params["phi"] = phi
-    plot_spectrum(ribbon_ZKM, phi, params, ax=ax)  
+    # k = np.pi
+    # fig, ax = plt.subplots(dpi=300)
+    # ax.set_title(f"Spectrum for H0 and k={k}")
+    # ax.set_xlabel(r"$\phi$")
+    # ax.set_ylabel(r"$E(\phi)$")
+    # ax.set_xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi]),
+    # ax.set_xticklabels([r"0", r"$\frac{\pi}{2}$", "$\pi$", r"$\frac{3\pi}{2}$","$2\pi$"])
+    # ax.grid()
+    # plt.tight_layout()
+    # ribbon_ZKM = make_Josephson_junction_ZKM(mu=mu, L=L)
+    # ribbon_ZKM = ribbon_ZKM.finalized()
+    # params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1, lambda_R=lambda_R, L=L, phi=phi, k=0, t_J=t_J)
+    # phi = np.linspace(0, 2*np.pi, 50)
+    # params["phi"] = phi
+    # plot_spectrum(ribbon_ZKM, phi, params, ax=ax)  
 
 if __name__ == '__main__':
     main()

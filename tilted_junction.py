89c3d216d370ee6c29d7e453747978c8245149d5
Gabriel Rodríguez Ruiz <gabrielrr96@gmail.com>
2022-03-09 18:55:33 -0300

    I changed the sign of lambda in Haniltonian following Aligia

--- tilted_junction.py
+++ tilted_junction.py
@@ -27,8 +27,8 @@ def onsite_Josephson_ZKM(site, mu, Delta_0, Delta_1, lambda_R, k, t, theta):
     if site.pos < [0.0]:
         theta = 0
     return ( (-2*t*np.cos(k) - mu) * np.kron(tau_z, np.eye(2)) +
-            (Delta_0 + 2*Delta_1*np.cos(k)) * np.kron(tau_x, np.eye(2))  +
-            2*lambda_R*np.sin(k) * ( np.cos(theta)*np.kron(tau_z, sigma_x) +
+            (Delta_0 + 2*Delta_1*np.cos(k)) * np.kron(tau_x, np.eye(2)) +
+            (-2)*lambda_R*np.sin(k) * ( np.cos(theta)*np.kron(tau_z, sigma_x) +
             np.sin(theta)*np.kron(tau_z, sigma_y) ) )
 
 def hopping_Josephson_ZKM(site1, site2, t, Delta_0, Delta_1,lambda_R, phi, k, t_J):
@@ -83,7 +83,7 @@ def Josephson_current(syst, params):
     current = np.diff(fundamental_energy) / dphi
     return current
 
-def plot_spectrum(syst, phi, params, ax=None):
+def plot_spectrum(params, k):
     """
     Plot the spectrum by changing the parameter 'phi'.
     Parameters
@@ -93,7 +93,19 @@ def plot_spectrum(syst, phi, params, ax=None):
     phi : np.array
         Phase difference between superconductors.
     """
-    kwant.plotter.spectrum(syst, ("phi", phi), params=params, ax=ax)
+    fig, ax = plt.subplots(dpi=300)
+    ax.set_title("Spectrum for H_ZKM")
+    ax.set_xlabel(r"$\phi$")
+    ax.set_ylabel(r"$J_k$")
+    ax.set_xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi]),
+    ax.set_xticklabels([r"0", r"$\frac{\pi}{2}$", "$\pi$", r"$\frac{3\pi}{2}$","$2\pi$"])
+    ax.grid()
+    plt.tight_layout()
+    ribbon_ZKM = make_Josephson_junction_ZKM()
+    ribbon_ZKM = ribbon_ZKM.finalized()
+    phi = np.linspace(0, 2*np.pi, 240)
+    params["k"] = k
+    kwant.plotter.spectrum(ribbon_ZKM, ("phi", phi), params=params, ax=ax)
 
 def plot_k_resolved_current(t, t_J, mu, Delta_0, Delta_1, lambda_R,
                             theta, L, k):
@@ -148,43 +160,6 @@ def plot_total_current(t, t_J, mu, Delta_0, Delta_1, lambda_R,
     plt.legend(loc="upper right")
     return fig, ax
 
-def main():
-    # without crossing 
-    t = 1
-    t_J = t/2
-    mu = 2*t
-    Delta_0 = 0.4*t
-    Delta_1 = 0.2*t
-    lambda_R = 0.5*t
-    
-    # with crossing
-    # t = 1
-    # t_J = t
-    # mu = t
-    # Delta_0 = 0.4*t
-    # Delta_1 = 0.4*t
-    # lambda_R = 0.5*t
-    
-    theta = np.pi/4
-    L = 100
-    k = np.linspace(-np.pi, 0, 240)
-    params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1,
-                  lambda_R=lambda_R, L=L,
-                  t_J=t_J, theta=theta)
-    
-    current = plot_k_resolved_current(k=k, **params)
-    np.save(f"k_current_L=100_no_crossing_theta={theta:.2}", current)
-    #fig.savefig(os.getcwd()+f"/Images/Tilted/Josephson_H_ZKM_crossing_L={L}_theta={theta:.2f}.png")
-
-    params.pop("theta")
-    theta = [0, np.pi/4, np.pi/2, np.pi, 3*np.pi/2, 7*np.pi/4]
-    #fig, ax = plot_total_current(k=k, theta=theta, **params)
-    #fig.savefig(os.getcwd()+f"/Images/Tilted/Total_Josephson_H_ZKM_without_crossing_L={L}.png")
-
-if __name__ == '__main__':
-    main()
-    print('\007')  # Ending bell
-
 def reshape_dat(current, phi, name):
     """
     Saves a .txt file with the right shape for grace
@@ -204,3 +179,45 @@ def reshape_dat(current, phi, name):
     current_bis = np.reshape(current_bis, (x+(x+1)*(y-1),1))
     result = np.append(phi, current_bis, axis=1)
     np.savetxt(name, result, fmt="%s")
+
+
+def main():
+    # without crossing 
+    t = 1
+    t_J = t/2
+    mu = 2*t
+    Delta_0 = 0.4*t
+    Delta_1 = 0.2*t
+    lambda_R = 0.5*t
+    
+    # with crossing
+    # t = 1
+    # t_J = t
+    # mu = t
+    # Delta_0 = 0.4*t
+    # Delta_1 = 0.4*t
+    # lambda_R = 0.5*t
+    
+    theta = 0
+    L = 10
+    # k = np.linspace(-np.pi, 0, 240)
+    k = np.linspace(-np.pi, 0, 10)
+
+    params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1,
+                  lambda_R=lambda_R, L=L,
+                  t_J=t_J, theta=theta)
+    
+    current = plot_k_resolved_current(k=k, **params)
+    #np.save(f"k_current_L=100_no_crossing_theta={theta:.2}", current)
+    #fig.savefig(os.getcwd()+f"/Images/Tilted/Josephson_H_ZKM_crossing_L={L}_theta={theta:.2f}.png")
+
+    params.pop("theta")
+    theta = [0, np.pi/4, np.pi/2, np.pi, 3*np.pi/2, 7*np.pi/4]
+    #fig, ax = plot_total_current(k=k, theta=theta, **params)
+    #fig.savefig(os.getcwd()+f"/Images/Tilted/Total_Josephson_H_ZKM_without_crossing_L={L}.png")
+    
+    
+if __name__ == '__main__':
+    main()
+    print('\007')  # Ending bell
+

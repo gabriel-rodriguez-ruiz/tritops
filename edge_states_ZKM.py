8f4211abe592da9c31f090f86d4b23a037918ca7
Gabriel Rodríguez Ruiz <gabrielrr96@gmail.com>
2022-03-24 21:42:57 -0300

    Now the edge modes have different color in spectrum.

--- edge_states_ZKM.py
+++ edge_states_ZKM.py
@@ -18,7 +18,7 @@ tau_y = np.array([[0, -1j], [1j, 0]])
 tau_z = np.array([[1, 0], [0, -1]])
 
 def Hamiltonian(t, k, mu, L, Delta_0, Delta_1, lambda_R):
-    r"""Hamiltonian for ZKM model with:
+    r"""Returns the H_k matrix for ZKM model with:
 
     .. math::
         H_{ZKM} = \frac{1}{2}\sum_k H_k
@@ -40,7 +40,7 @@ def Hamiltonian(t, k, mu, L, Delta_0, Delta_1, lambda_R):
     matrix_diagonal = np.kron(np.eye(L), onsite)     #diagonal part of matrix
     matrix_outside_diagonal = np.block([ [np.zeros((4*(L-1),4)),np.kron(np.eye(L-1), hopping)],
                                          [np.zeros((4,4*L))] ])     #upper diagonal part
-    matrix = 1/2*(matrix_diagonal + matrix_outside_diagonal + matrix_outside_diagonal.conj().T)
+    matrix = (matrix_diagonal + matrix_outside_diagonal + matrix_outside_diagonal.conj().T)
     return matrix
 # without crossing 
 t = 1
@@ -58,26 +58,42 @@ eigenvalues, eigenvectors = np.linalg.eigh(H)
 # Sort according to the absolute values of energy
 eigenvectors = eigenvectors[:, np.argsort(np.abs(eigenvalues))]
 eigenvalues = eigenvalues[np.argsort(np.abs(eigenvalues))]
-edge_mode = eigenvectors[::4,0]
 
 #%% Spectrum
 def spectrum(k_values, **params):
-    """Returns an array whose columns are the eigenvalues of the system for
+    """Returns an array whose rows are the eigenvalues of the system for
     for a definite k.
     """
-    eigenvalues = np.array([[]])
+    eigenvalues = []
     for k in k_values:
         params["k"] = k
         H = Hamiltonian(**params)
-        eigenvalues.append(np.linalg.eigvals(H))
+        energies = np.linalg.eigvalsh(H)
+        energies = list(energies)
+        eigenvalues.append(energies)
+    eigenvalues = np.array(eigenvalues)
     return eigenvalues
-k = np.linspace(0, np.pi)
-fig, ax = plt.subplots(dpi=300)
-ax.plot(k, [spectrum(value, **params) for value in k], linestyle="None",
-        marker=".", markersize=0.5)
+k = np.linspace(0, np.pi, 100)
+spectrum = spectrum(k, **params)  	
+#%% Plotting
 
-#%% Edge state
-fig, ax = plt.subplots(dpi=300)
-ax.plot(abs(eigenvectors[0::2,0])**2)
+fig, ax = plt.subplots(figsize=(4,3))
+#fig.set_figwidth(246/72)    # in inches, \columnwith=246pt and 1pt=1/72 inch
+plt.rc('font', family='serif')  #set font family
+plt.rc('xtick', labelsize='small') # reduced tick label size
+plt.rc('ytick', labelsize='small')
+plt.rc('text', usetex=True)
+ax.plot(k, spectrum, marker=".", markersize=0.5,
+        color="m") #each column in spectrum is a separate dataset
+ax.plot(k, spectrum[:,398:402], marker=".", markersize=0.5,
+        color="c") #each column in spectrum is a separate dataset
 
-
+ax.set_ylim((-3, 3))
+ax.set_xlim((0, np.pi))
+ax.set_xticks(np.arange(0,1.2,step=0.2)*np.pi)
+ax.set_xticklabels(["0"]+list(np.array(np.round(np.arange(0.2,1,step=0.2),1), dtype=str))+["1"])
+ax.set_xticks(np.arange(0,1.1,step=0.1)*np.pi)
+ax.set_xlabel(r"$k_z/\pi$")
+ax.set_ylabel(r"$E(k_z)$")
+ax.set_yticks(np.arange(-2,3,step=1))
+plt.tight_layout()

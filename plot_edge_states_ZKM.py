ecbe510e13d04a190fb441b16f2ea16d91691d38
Gabriel <gabrielrr96@gmail.com>
2022-04-26 17:18:34 -0300

    Nothing.

--- plot_edge_states_ZKM.py
+++ plot_edge_states_ZKM.py
@@ -83,7 +83,7 @@ Delta_0 = 4*t
 Delta_1 = 2.2*t
 lambda_R = 7*t
 
-k = 0.99*np.pi
+k = np.pi
 L = 200
 theta = 0
 n = 0
@@ -107,15 +107,78 @@ plt.title("Distribution probability for spin up electron")
 
 #%% Spin structure
 
-def spin_structure(k, params):
-    """ Returns the phase phi and angle theta for the edge modes.
+def edge_states(k, params):
+    """ Returns the edge states localized at the edges in a matrix whose columns
+     are [left_minus, left_plus, right_minus, right_plus].
     """
     H = Hamiltonian(k=k, **params)
     eigenvalues, eigenvectors = np.linalg.eigh(H)
-    # Sort according to the absolute values of energy
-    eigenvalues = eigenvalues[np.argsort(np.abs(eigenvalues))]
-    eigenvectors = eigenvectors[:, np.argsort(np.abs(eigenvalues))]
-    # Edge right minus
-    right_minus= eigenvectors[-4:, 0]
-    theta_k = 2*np.arctan(np.abs(right_minus[1]/right_minus[0]))
-    return theta_k
+    eigenvectors = eigenvectors[:,(2*L-2):(2*L+2)]  # I look only zero energy modes
+    states = [0,0,0,0]
+    for i in range(4):
+        if (np.abs(eigenvectors[0,i]) > 0.01): #If it is a left edge state
+            if i<2:
+                left_minus = eigenvectors[:4,i]
+                states[0] = left_minus
+            else:
+                left_plus = eigenvectors[:4,i]
+                states[1] = left_plus
+        else:
+            if i<2:
+                right_minus = eigenvectors[4*L-4:,i]
+                states[2] = right_minus
+            else:
+                right_plus = eigenvectors[4*L-4:,i]
+                states[3] = right_plus
+    return np.array(states).T
+
+def spin_structure(k_values, params):
+    """
+    Returns the angle theta_k and phase phi_nu_k for all values of k.
+    """
+    theta_k = []
+    phi_r_k = []
+    phi_l_k = []
+    delta_l_k = []
+    for k_value in k_values:
+        print(k_value)
+        states = edge_states(k_value, params)
+        right_minus = states[:,2]
+        left_minus = states[:,0]
+        theta_k.append(2*np.arctan(np.abs(right_minus[0])/np.abs(right_minus[1])))
+        phi_r_k.append(np.angle(-right_minus[0]/right_minus[1]))
+        phi_l_k.append(np.angle(-left_minus[0]/left_minus[1]))
+
+    return theta_k, phi_r_k, phi_l_k, delta_l_k
+#Aligia
+t = 1
+mu = 2*t
+Delta_0 = 4*t
+Delta_1 = 2.2*t
+lambda_R = 7*t
+
+k = 0.8*np.pi
+L = 300
+theta = 0
+n = 0
+params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1,
+              lambda_R=lambda_R, L=L, theta=theta)
+
+H = Hamiltonian(k=k, **params)
+eigenvalues, eigenvectors = np.linalg.eigh(H)
+eigenvectors = eigenvectors[:,(2*L-2):(2*L+2)]
+right_minus = eigenvectors[4*L-4:,0]    # depends on parameters
+theta_k = 2*np.arctan(np.abs(right_minus[0])/np.abs(right_minus[1]))
+print(theta_k)
+phi_r_k = np.angle(-right_minus[0]/right_minus[1])
+print(phi_r_k)
+
+#states = edge_states(k, params)
+
+k_values = np.linspace(0.8*np.pi, np.pi-0.001)
+
+theta_k, phi_r_k, phi_l_k, delta_l_k = spin_structure(k_values=k_values, params=params)
+
+fig, ax = plt.subplots()
+ax.plot(k_values, phi_r_k)
+#ax.plot(k_values, phi_l_k)

a096d9adf50ae9f2aaebcb68c01a3a362145b014
Gabriel Rodríguez Ruiz <gabrielrr96@gmail.com>
2022-05-01 12:32:00 -0300

    TRITOPS-S figures are working.

--- tritops_s.py
+++ tritops_s.py
@@ -47,7 +47,7 @@ print('\007')  # Ending bell
 
 #%% Plotting
 
-current = current_Eu_s
+current = current_ZKM_s
 
 phi = np.linspace(0, 2*np.pi, 240)
 plt.rc('text', usetex=False)
@@ -66,6 +66,7 @@ plt.tight_layout()
 #%% all three TRITOPS-S
 current_A1u_s = np.load("k_current_TRITOPS_S_A1u_L_100_Delta0_0.4_Delta1_0.2_lambda_0.5_mu_2_tJ_0.5_theta_0.npy")
 current_Eu_s = np.load("k_current_TRITOPS_S_Eu_L_100_Delta0_0.4_Delta1_0.2_lambda_0.5_mu_2_tJ_0.5_theta_0.npy")
+current_ZKM_s = np.load("k_current_TRITOPS_S_ZKM_L_100_Delta0_0.4_Delta1_0.2_lambda_0.5_mu_2_tJ_0.5_theta_0.npy")
 
 phi = np.linspace(0, 2*np.pi, 240)
 
@@ -79,30 +80,51 @@ plt.rcParams['xtick.labeltop'] = False
 plt.rcParams['ytick.right'] = True    #ticks on left
 plt.rcParams['ytick.labelright'] = False
 
-fig = plt.figure(figsize=(4,6), dpi=300)
+plt.close()
+
+fig = plt.figure(figsize=(4,3), dpi=300)
 gs = fig.add_gridspec(3, hspace=0)
 axs = gs.subplots(sharex=True, sharey=True)
-axs[0].plot(phi, current_A1u_s.T, linewidth=0.1)
-axs[1].plot(phi, current_Eu_s.T, linewidth=0.1)
-#axs[2].plot(x, y, '+')
+axs[0].plot(phi, current_A1u_s.T[:, 0], linewidth=1, color="c")
+axs[0].plot(phi, current_A1u_s.T[:, 1:30], linewidth=0.1, color="m")
+axs[0].plot(phi, current_A1u_s.T[:, 30:], linewidth=0.1, color="r")
+
+axs[1].plot(phi, current_Eu_s.T[:, 0], linewidth=1, color="c")
+axs[1].plot(phi, current_Eu_s.T[:, 1:30], linewidth=0.1, color="m")
+axs[1].plot(phi, current_Eu_s.T[:, 30:], linewidth=0.1, color="r")
+
+axs[2].plot(phi, current_ZKM_s.T[:, 0], linewidth=1, color="c")
+axs[2].plot(phi, current_ZKM_s.T[:, 1:30], linewidth=0.1, color="m")
+axs[2].plot(phi, current_ZKM_s.T[:, 30:], linewidth=0.1, color="r")
 
 axs[0].set_xlabel(r"$\Phi/\pi$")
 axs[0].set_ylabel(r"$J(k)$")
 axs[0].set_xlim((0, 2*np.pi))
-axs[0].set_ylim((-0.15, 0.15))
+axs[0].set_ylim((-0.12, 0.12))
 axs[0].set_xticks(np.arange(0,2.5,step=0.5)*np.pi)
 axs[0].set_xticklabels(["0"]+list(np.array(np.round(np.arange(0.5,2,step=0.5),1), dtype=str)) + ["2"])
 axs[0].set_xticks(np.arange(0,2,step=0.25)*np.pi, minor=True)
 axs[0].set_yticks(np.arange(-0.1,0.15,step=0.1))
-axs[0].set_yticks(np.arange(-0.15,0.2,step=0.05), minor=True)
+axs[0].set_yticks(np.arange(-0.1,0.1,step=0.05), minor=True)
 
 axs[1].set_xlabel(r"$\Phi/\pi$")
 axs[1].set_ylabel(r"$J(k)$")
 axs[1].set_xlim((0, 2*np.pi))
-axs[1].set_ylim((-0.15, 0.15))
+#axs[1].set_ylim((-0.15, 0.15))
 axs[1].set_xticks(np.arange(0,2.5,step=0.5)*np.pi)
 axs[1].set_xticklabels(["0"]+list(np.array(np.round(np.arange(0.5,2,step=0.5),1), dtype=str)) + ["2"])
 axs[1].set_xticks(np.arange(0,2,step=0.25)*np.pi, minor=True)
-axs[1].set_yticks(np.arange(-0.1,0.15,step=0.1))
-axs[1].set_yticks(np.arange(-0.15,0.2,step=0.05), minor=True)
+#axs[1].set_yticks(np.arange(-0.1,0.15,step=0.1))
+#axs[1].set_yticks(np.arange(-0.15,0.2,step=0.05), minor=True)
 
+axs[2].set_xlabel(r"$\Phi/\pi$")
+axs[2].set_ylabel(r"$J(k)$")
+axs[2].set_xlim((0, 2*np.pi))
+#axs[2].set_ylim((-0.15, 0.15))
+axs[2].set_xticks(np.arange(0,2.5,step=0.5)*np.pi)
+axs[2].set_xticklabels(["0"]+list(np.array(np.round(np.arange(0.5,2,step=0.5),1), dtype=str)) + ["2"])
+axs[2].set_xticks(np.arange(0,2,step=0.25)*np.pi, minor=True)
+#axs[2].set_yticks(np.arange(-0.1,0.15,step=0.1))
+#axs[2].set_yticks(np.arange(-0.15,0.2,step=0.05), minor=True)
+
+plt.tight_layout()

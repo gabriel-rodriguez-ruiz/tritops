15b5141a80c7fc1b56b89c3df316d4d458e5a773
Gabriel Rodríguez Ruiz <gabrielrr96@gmail.com>
2022-05-23 13:01:30 -0300

    I changed the labels of some figures.

--- figure6_pi_over_4.py
+++ figure6_pi_over_4.py
@@ -41,6 +41,8 @@ params = dict(t=t, mu=mu, Delta_0=Delta_0, Delta_1=Delta_1,
 print('\007')  # Ending bell
 
 #%%
+from effective_hamiltonian import parameters, effective_current
+
 current = np.load("k_current_L_200_Delta0_0.4_Delta1_0.2_lambda_0.5_mu_2_tJ_0.5_theta_pi_over_4.npy")
 #current_bis = np.load("k_current_L_200_Delta0_0.4_Delta1_0.2_lambda_0.5_mu_1_tJ_0.5_theta_0.npy")
 
@@ -64,8 +66,8 @@ ax.plot(phi, current.T[:,20:], linewidth=0.1, color="r")
 # ax.plot(phi, current.T[:,1:50], linewidth=0.1, color="m")
 # ax.plot(phi, current.T[:,50:], linewidth=0.1, color="r")
 
-ax.set_xlabel(r"$\Phi/\pi$")
-ax.set_ylabel(r"$J(k)$")
+ax.set_xlabel(r"$\phi/\pi$")
+ax.set_ylabel(r"$J_k(\phi)[e/\hbar]$")
 ax.set_xlim((0, 2*np.pi))
 ax.set_ylim((-0.15, 0.15))
 ax.set_xticks(np.arange(0,2.5,step=0.5)*np.pi)
@@ -86,8 +88,7 @@ ax.set_yticks(np.arange(-0.15,0.2,step=0.05), minor=True)
 
 plt.tight_layout()
 
-ax3 = fig.add_subplot()
-from effective_hamiltonian import parameters, effective_current
+#ax3 = fig.add_subplot()
 
 # without crossing 
 t = 1
@@ -104,9 +105,15 @@ phi_values = np.linspace(0, 2*np.pi, 240)
 k = np.linspace(-np.pi, 0, 75)[1:11]
 
 for k in k:
-    effective = ax3.plot(phi, [1.25*effective_current(k, phi, theta, t_J=t_J, lambda_R=lambda_R, w2=parameters(k)[0], phi_k=parameters(k)[2], rho_k=parameters(k)[1]) for phi in phi_values],
+    effective = ax3.plot(phi, [1.25*effective_current(k, phi, theta, t_J=t_J, lambda_R=lambda_R,
+                                                      w2=parameters(k, lambda_R, Delta_0, Delta_1, mu, t)[0],
+                                                      phi_k=parameters(k, lambda_R, Delta_0, Delta_1, mu, t)[2],
+                                                      rho_k=parameters(k, lambda_R, Delta_0, Delta_1, mu, t)[1]) for phi in phi_values],
             label=f"{k:.2f}", linestyle = "dashed", linewidth=0.5, color="m")
-ax3.plot(phi, [1.25*effective_current(-np.pi, phi, theta, t_J=t_J, lambda_R=lambda_R, w2=parameters(-np.pi)[0], phi_k=parameters(-np.pi)[2], rho_k=parameters(-np.pi)[1]) for phi in phi_values],
+ax3.plot(phi, [1.25*effective_current(-np.pi, phi, theta, t_J=t_J, lambda_R=lambda_R,
+                                      w2=parameters(-np.pi, lambda_R, Delta_0, Delta_1, mu, t)[0],
+                                      phi_k=parameters(-np.pi, lambda_R, Delta_0, Delta_1, mu, t)[2],
+                                      rho_k=parameters(-np.pi, lambda_R, Delta_0, Delta_1, mu, t)[1]) for phi in phi_values],
         label=f"{k:.2f}", linestyle = "dashed", linewidth=0.5, color="c")
 
 ax3.plot(phi_values, current.T[:,0], linewidth=1, color="c")
@@ -118,8 +125,8 @@ ax3.set_ylim(-0.02, 0.15)
 ax3.set_xticks(np.arange(0,1.5,step=0.5)*np.pi)
 ax3.set_xticklabels(["0"]+ ["0.5"] + ["1"])
 ax3.set_xticks(np.arange(0,1,step=0.25)*np.pi, minor=True)
-ax3.set_xlabel(r"$\Phi/\pi$", labelpad=0)
-ax3.set_ylabel(r"$J(k)$", labelpad=0)
+ax3.set_xlabel(r"$\phi/\pi$", labelpad=0)
+ax3.set_ylabel(r"$J_k(\phi)[e/\hbar]$", labelpad=0)
 ax3.set_yticks(np.arange(0,0.15,step=0.1))
 ax3.set_yticks(np.arange(0,0.2,step=0.05), minor=True)
 ax3.set_yticklabels(["0", "0.1"])

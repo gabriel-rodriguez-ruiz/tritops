7558165e5260437553fc67ca112de7fda461bb01
Gabriel Rodríguez Ruiz <gabrielrr96@gmail.com>
2022-06-09 10:17:26 -0300

    I considered and defined majorana in square.

--- Eu_square.py
+++ Eu_square.py
@@ -33,7 +33,7 @@ def Hamiltonian_Eu(t, mu, L_x, L_y, Delta):
        \vec{c} = (c_{k,\uparrow}, c_{k,\downarrow},c^\dagger_{-k,\downarrow},-c^\dagger_{-k,\uparrow})^T
     """
     onsite = -mu * np.kron(tau_z, sigma_0)
-    hopping_x = -t*np.kron(tau_z, sigma_0) + 1j*Delta/2 * np.kron(tau_x, sigma_z)
+    hopping_x = -t*np.kron(tau_z, sigma_0) - 1j*Delta/2 * np.kron(tau_x, sigma_z)
     hopping_y = -t*np.kron(tau_z, sigma_0) - 1j*Delta/2 * np.kron(tau_x, sigma_z)
     matrix_diagonal = np.kron(np.eye(L_x*L_y), onsite)     #diagonal part of matrix
     matrix_outside_diagonal_x = np.block([ [np.zeros((4*(L_x-1)*L_y,4*L_y)),np.kron(np.eye((L_x-1)*L_y), hopping_x)],
@@ -50,7 +50,7 @@ def Hamiltonian_Eu(t, mu, L_x, L_y, Delta):
 
 t = 1
 Delta = 1
-mu = -3   #mu = -3  entre -4t y 0 hay estados de borde
+mu = -2   #mu = -3  entre -4t y 0 hay estados de borde
 
 L_x = 20
 L_y = 20
@@ -71,5 +71,20 @@ majorana_down_plus = (zero_modes[1::4,2]+1j*zero_modes[2::4,2]).reshape((L_x,L_y
 #plt.imshow(np.abs(zero_mode_up))
 #plt.imshow(np.abs(majorana_up))
 plt.imshow(np.abs(majorana_up_plus))
-
+plt.title(rf"$\mu$={mu}t")
 plt.colorbar()
+    
+#plt.figure()
+#plt.plot(eigenvalues)
+#%%
+def plot_majorana(mu):
+    H = Hamiltonian_Eu(t, mu, L_x, L_y, Delta)
+    eigenvalues, eigenvectors = np.linalg.eigh(H)
+    zero_modes = eigenvectors[:,(2*L_x*L_y-2):(2*L_x*L_y+2)]    # I extract the eigenvectors asociated to zero energy
+    majorana_up_plus = (zero_modes[0::4,2]+1j*zero_modes[3::4,2]).reshape((L_x,L_y))
+    plt.imshow(np.abs(majorana_up_plus))
+    plt.title(rf"$\mu$={mu}t")
+    plt.colorbar()
+    
+
+plot_majorana(mu)
